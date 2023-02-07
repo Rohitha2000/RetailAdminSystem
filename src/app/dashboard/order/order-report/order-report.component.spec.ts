6ad3122b3018d3e05796578e0c9412e39b926ba8
rohitha <gourabathunirohitha@gmail.com>
2023-02-07 23:27:57 +0530

    added test cases

--- src/app/dashboard/order/order-report/order-report.component.spec.ts
+++ src/app/dashboard/order/order-report/order-report.component.spec.ts
@@ -1,18 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { ProductService } from '../../product/service/product.service';
+import { OrderService } from '../service/order.service';
 
 import { OrderReportComponent } from './order-report.component';
 
 describe('OrderReportComponent', () => {
   let component: OrderReportComponent;
   let fixture: ComponentFixture<OrderReportComponent>;
+  let service: OrderService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OrderReportComponent ]
+      declarations: [ OrderReportComponent ],
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers:[OrderService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(OrderReportComponent);
+     service= TestBed.inject(OrderService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,4 +29,24 @@ describe('OrderReportComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('Fetch data ', ()=>{
+    const obj= [{
+      "id": 1,
+      "company_name": "biscuit",
+      "company_description": "biscuit category",
+      "company_id": "23"
+    },
+    {
+      "id": 23,
+      "company_name": "chocolate",
+      "company_description": "Dairy Category",
+      "company_id": "300"
+    }];
+    
+    spyOn(service, 'getOrders').and.returnValue(of(obj));
+   
+    component.getOrderdata();
+    expect(service.getOrders).toHaveBeenCalled();
+    expect(obj).toEqual(component.dataSource);
+  })
 });
--- src/app/dashboard/product/product-report/product-report.component.spec.ts
+++ src/app/dashboard/product/product-report/product-report.component.spec.ts
@@ -1,18 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { ProductService } from '../service/product.service';
 
 import { ProductReportComponent } from './product-report.component';
 
-describe('ProductCategoryComponent', () => {
+describe('ProductReportComponent', () => {
   let component: ProductReportComponent;
   let fixture: ComponentFixture<ProductReportComponent>;
+  let service: ProductService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductReportComponent ]
+      declarations: [ ProductReportComponent ],
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers:[ProductService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ProductReportComponent);
+    service= TestBed.inject(ProductService)
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,4 +28,47 @@ describe('ProductCategoryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
+  it('delete Company - deleteCompany()', ()=>{
+    const obj= [{
+      "id": 1,
+      "company_name": "biscuit",
+      "company_description": "biscuit category",
+      "company_id": "23"
+    },
+    {
+      "id": 23,
+      "company_name": "chocolate",
+      "company_description": "Dairy Category",
+      "company_id": "300"
+    }];
+    spyOn(component,'fetchdata').and.callThrough();
+    spyOn(service, 'delete').and.returnValue(of(null));
+    component.dataSource= obj;
+    component.deleteProduct(1);
+    expect(component.fetchdata).toHaveBeenCalled();
+    expect(service.delete).toHaveBeenCalledWith(1);
+  })
+
+  it('Fetch data ', ()=>{
+    const obj= [{
+      "id": 1,
+      "company_name": "biscuit",
+      "company_description": "biscuit category",
+      "company_id": "23"
+    },
+    {
+      "id": 23,
+      "company_name": "chocolate",
+      "company_description": "Dairy Category",
+      "company_id": "300"
+    }];
+    
+    spyOn(service, 'getProducts').and.returnValue(of(obj));
+   
+    component.fetchdata();
+    expect(service.getProducts).toHaveBeenCalled();
+    expect(obj).toEqual(component.dataSource);
+  })
 });
